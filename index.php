aa31553b0a17f8cca177766bdf3e90ffaf64ffa7
formagrey <formagrey@gmail.com>
2018-02-14 16:28:42 +0100

    formulaire0

--- index.php
+++ index.php
@@ -9,10 +9,9 @@
     <body>
         <div class="container-fluid">
             <div class="row col-12">
-                <div class="col-12 text-center animated fadeInDown">
+                <div class="col-12 text-center">
                     <h1>BLOG en PHP MYSQL</h1>
-
-                    <nav class="navbar navbar-expand-md navbar-dark bg-dark col-12">
+                      <nav class="navbar navbar-expand-md navbar-dark bg-dark col-12">
                       <a class="navbar-brand" href="#"></a>
                       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                           <span class="navbar-toggler-icon"></span>
