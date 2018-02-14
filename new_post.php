ffdc5bff59d7dc121953987700b69a500cc87d76
formagrey <formagrey@gmail.com>
2018-02-14 14:19:53 +0100

    mercredi post1

--- new_post.php
+++ new_post.php
@@ -17,31 +17,28 @@ $texte = $_POST["texte"] ;
 //Variables Catégories
 $cat = $_POST["cat"]
 
-//création de la requête SQL:
-
-//test utilisateur:
+//création des requêtes SQL:
+//test utilisateur par son adresse mail:
 $testUtilisateur = mysqli_query($link,
 	"SELECT mail
 	FROM Auteur
-	WHERE mail = '$mail'
-    ");
+	WHERE mail = '$mail'");
     $res_test_user = mysqli_num_rows($testUtilisateur);
 
-if($res_test_user == 0)
-{
-	$new_auteur = mysqli_query($link,
-	"INSERT INTO Auteur (nom, prenom, mail)
-	VALUES ('$nom', '$prenom', '$mail')");
+if($res_test_user == 0) // si test utilisateur est à alors l'utilisateur n'existe pas donc on va le créer
+    {
+    	$new_auteur = mysqli_query($link,
+    	"INSERT INTO Auteur (nom, prenom, mail)
+    	VALUES ('$nom', '$prenom', '$mail')");
+    }
 
-}
-
-	$res = mysqli_query($link,
-	"SELECT id
-	FROM Auteur
-	WHERE mail = '$mail'
-	");
-    $res_auteur = mysqli_fetch_assoc($res);
-    $id_auteur = $res_auteur['id'];
+    	$res = mysqli_query($link,
+    	"SELECT id
+    	FROM Auteur
+    	WHERE mail = '$mail'
+    	");
+        $res_auteur = mysqli_fetch_assoc($res);
+        $id_auteur = $res_auteur['id'];
 
 
 $requete = mysqli_query($link,
@@ -49,13 +46,13 @@ $requete = mysqli_query($link,
 	VALUES ('$titre', '$texte', '$id_auteur')");
 
 //affichage des résultats, pour savoir si l'insertion a marchée:
-if($requete)
-    {
-      echo("L'insertion a été correctement effectuée") ;
-      header("Location: index.php");
-    }
-else
-    {
-      echo("L'insertion à échouée" . mysqli_error($link)) ;
-    }
+    if($requete)
+        {
+          echo("L'insertion a été correctement effectuée") ;
+          header("Location: index.php");
+        }
+    else
+        {
+          echo("L'insertion à échouée" . mysqli_error($link)) ;
+        }
  ?>
